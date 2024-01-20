f12189c04127516b114442f99794dc4db1f59db7
LianLambert <lian1lambert@gmail.com>
2024-01-20 07:31:50 -0500

    Player Movement and Bullet Streams

--- McGameJam2024/Assets/Scripts/Player.cs
+++ McGameJam2024/Assets/Scripts/Player.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private Rigidbody2D rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateMovement();
     }
+
+
+    void UpdateMovement() {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += moveSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+
 }
